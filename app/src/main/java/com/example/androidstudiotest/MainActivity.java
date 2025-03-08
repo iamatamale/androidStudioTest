8e17fdca59cbb9458a4721323f8c297c8900c3e4
Ryan <ryan.vick@my.utsa.edu>
2025-03-08 12:31:22 -0600

    Added function for buttons to swap pages

--- app/src/main/java/com/example/androidstudiotest/MainActivity.java
+++ app/src/main/java/com/example/androidstudiotest/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.androidstudiotest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button movetoPageOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        movetoPageOne=findViewById(R.id.moveToPage1);
+        movetoPageOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              Intent intent = new Intent(MainActivity.this,page1.class);
+              startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/androidstudiotest/page1.java
+++ app/src/main/java/com/example/androidstudiotest/page1.java
@@ -1,6 +1,9 @@
 package com.example.androidstudiotest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class page1 extends AppCompatActivity {
-
+    private Button returnToStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,14 @@ public class page1 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        returnToStart = findViewById(R.id.moveToStart);
+        returnToStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(page1.this,MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
